ad42092fafecef6f4080937a21839144276d628e
BRUNO AQUILES DE LIMA <bal22@inf.ufpr.br>
2023-04-17 18:27:35 -0300

    NvimTree transparente 2.0

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -3,5 +3,4 @@ require("catppuccin").load("frappe")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" } )
-
-vim.cmd[[hi NvimTreeNormal guibg=NONE ctermbg=NONE]]
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" } )
