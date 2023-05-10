44ec2db288f28fca9c878cf9aed9e40be95e34ab
Aquiles-bb <brunoaquiles@ufpr.br>
2023-05-10 08:15:55 -0300

    locality

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,6 +23,7 @@ lsp.setup()
 
 local cmp = require('cmp')
 local lspkind = require('lspkind')
+-- local kind_mapper = {6, 3, 2, 22, 4, 5, 1, 7, 8, 9, 10, 11, 12, 13, 14}
 
 local cmp_config = lsp.defaults.cmp_config({
     window = {
@@ -32,7 +33,7 @@ local cmp_config = lsp.defaults.cmp_config({
     },
 
     sources = {
-        {name = "nvim_lsp", keyword_length = 1},
+        {name = "nvim_lsp", keyword_length = 2},
         {name = "luasnip", keyword_length = 2},
         {name = "path", keyword_length = 2},
         {name = "buffer", keyword_length = 3},
