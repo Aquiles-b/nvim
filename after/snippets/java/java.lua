809fafe21eca1ecdecb92e4bcc093b9ebbf4327d
BRUNO AQUILES DE LIMA <bal22@inf.ufpr.br>
2023-08-02 17:26:27 -0300

    Novo snippet java

--- after/snippets/java/java.lua
+++ after/snippets/java/java.lua
@@ -14,4 +14,12 @@ return {
     {
         t("System.out.println("), i(0), t(");")
     }),
+
+    s({
+        trig = "sopf",
+        dscr = "Formatado",
+    },
+    {
+        t('System.out.printf("'), i(1), t('", '), i(2), t(');')
+    }),
 }
