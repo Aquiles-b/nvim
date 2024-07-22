22caaf8e42b9211cbe6c58a10806a5b4bb6e423a
Aquiles <brunoaquiles@ufpr.br>
2024-07-21 22:53:26 -0300

    [FIX] latex liberado no tree sitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -41,7 +41,6 @@ lazy.setup({
                 auto_install = true,
                 highlight = {
                     enable = true,
-                    disable = { "latex" },
                     additional_vim_regex_highlighting = false,
                 },
             })
